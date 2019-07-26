4d454149d53d571d02ef2c55d0986de0fc843ad5
wakatakeru <s15t233@stu.kagawa-u.ac.jp>
2019-07-26 20:02:23 +0900

    add

--- setup.sh
+++ setup.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-cd /tmp
 yum install -y kernel-devel kernel-headers dkms wget
-wget http://download.virtualbox.org/virtualbox/6.0.10/VirtualBox-6.0-6.0.10_132072_el7-1.x86_64.rpm
-yum install -y VirtualBox-6.0-6.0.10_132072_el7-1.x86_64.rpm
+wget http://download.virtualbox.org/virtualbox/6.0.10/VirtualBox-6.0-6.0.10_132072_el7-1.x86_64.rpm -P /tmp
+yum install -y /tmp/VirtualBox-6.0-6.0.10_132072_el7-1.x86_64.rpm
 
 rpm -Uvh https://releases.hashicorp.com/vagrant/2.2.5/vagrant_2.2.5_x86_64.rpm
 
-bundle install
-
+bundle install --path=.bundle
 vagrant plugin install vagrant-itamae
