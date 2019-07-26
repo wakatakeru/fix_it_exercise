1c798224aa53f9c77e1f48b753d30450f87631be
wakatakeru <s15t233@stu.kagawa-u.ac.jp>
2019-07-26 20:10:35 +0900

    add

--- setup.sh
+++ setup.sh
@@ -6,5 +6,7 @@ yum install -y /tmp/VirtualBox-6.0-6.0.10_132072_el7-1.x86_64.rpm
 
 rpm -Uvh https://releases.hashicorp.com/vagrant/2.2.5/vagrant_2.2.5_x86_64.rpm
 
+gem install bundler
+
 bundle install --path=.bundle
 vagrant plugin install vagrant-itamae
