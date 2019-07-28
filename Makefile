dd7e52b4bb1b1204ec8806305a240df358aa45d1
wakatakeru <s19g460@stu.kagawa-u.ac.jp>
2019-07-28 15:29:50 +0900

    Add/setup script (#7)
    * add

---
+++ Makefile
@@ -0,0 +1,11 @@
+setup: setup.sh 
+	./setup.sh
+	vagrant plugin install vagrant-itamae
+start:
+	vagrant up
+stop:
+	vagrant halt
+test:
+	bundle exec rake
+clean:
+	yes | vagrant destroy
---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+yum install -y kernel-devel kernel-headers dkms wget
+wget http://download.virtualbox.org/virtualbox/6.0.10/VirtualBox-6.0-6.0.10_132072_el7-1.x86_64.rpm -P /tmp
+yum install -y /tmp/VirtualBox-6.0-6.0.10_132072_el7-1.x86_64.rpm
+
+rpm -Uvh https://releases.hashicorp.com/vagrant/2.2.5/vagrant_2.2.5_x86_64.rpm
+
+gem install bundler
+
+bundle install --path=.bundle
+
